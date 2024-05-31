83b04f9dcd31f465babe444f670c41b536c15795
Deepankar <deepankar.4net@gmail.com>
2024-06-01 03:54:46 +0530

    Second commit

--- JenkinsDemo/src/main/java/com/jenkins/JenkinsDemoApplication.java
+++ JenkinsDemo/src/main/java/com/jenkins/JenkinsDemoApplication.java
@@ -4,20 +4,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
-
 @SpringBootApplication
 public class JenkinsDemoApplication {
 	
 	public static org.slf4j.Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
 	
-	@PostConstruct
-	public void init() {
-		logger.info("Application started......");
-	}
-
 	public static void main(String[] args) {
-		logger.info("Application executed......");
+		logger.info("Application started");
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
--- JenkinsDemo/src/test/java/com/jenkins/JenkinsDemoApplicationTests.java
+++ JenkinsDemo/src/test/java/com/jenkins/JenkinsDemoApplicationTests.java
@@ -1,22 +1,21 @@
 package com.jenkins;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
-@SpringBootTest
 @RunWith(SpringRunner.class)
+@SpringBootTest
 class JenkinsDemoApplicationTests {
 	
 	public static org.slf4j.Logger logger = LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
-
 	@Test
 	void contextLoads() {
-		logger.info("Test cases are executing......");
+		logger.info("In test class");
 		assertEquals(true, true);
 	}
 
